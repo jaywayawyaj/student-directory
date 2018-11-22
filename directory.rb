9ed78a48456190a8f4898426779627880f5fe680
Jethro Whaley <jethro.whaley@gmail.com>
2018-11-22 18:16:58 +0000

    going back to add exercises - first update includes exercise 1,2, 6

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
-@students = []
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
   # get the first name
   name = gets.chomp
   puts "Enter the cohort you are in"
@@ -10,8 +10,8 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student and cohort hash to the array
-    @students << {name: name, cohort: cohort.capitalize.to_sym}
-    puts "Now we have #{@students.count} students"
+    students << {name: name, cohort: cohort.capitalize.to_sym}
+    puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
     # if a name is entered find out their cohort
@@ -21,80 +21,32 @@ def input_students
     end
   end
   # return the array of students and cohorts
-  @students
-end
-
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
-  # 2. read the input and save it into a variable
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-  when "1"
-    @students = input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "9"
-    exit # this will cause the program to terminate
-  else
-    puts "I don't know what you meant, try again"
-  end
-end
-
-def my_puts(string)
-  puts string.center(50)
+  students
 end
 
 def print_header
-  my_puts("The students of Villains Academy")
-  my_puts("-------------")
+  center_puts("The students of Villains Academy")
+  center_puts("-------------")
 end
 
-def print_students_list
-  @students.each.with_index(1) do |student, x|
-    my_puts("#{x}. #{student[:name]} (#{student[:cohort]} cohort)")
+def print(students)
+  students.each.with_index(1) do |student, x|
+    if student[:name][0] == "h"
+      center_puts("#{x} #{student[:name]} (#{student[:cohort]} cohort)")
+    end
   end
 end
 
-def print_footer
-  if @students.count == 1
-    my_puts("Overall, we have #{@students.count} great student")
-  elsif @students.count > 1
-    my_puts("Overall, we have #{@students.count} great students")
-  else
-    my_puts("There are no students at Villains Academy")
-  end
+def print_footer(students)
+  center_puts("Overall, we have #{students.count} great students")
 end
 
-interactive_menu
+def center_puts(string)
+  puts string.center(50)
+end
+
+students = input_students
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
